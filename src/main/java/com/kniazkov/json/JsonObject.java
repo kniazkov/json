8c094c4e4b3d74e5ec7e6faa5c46ff94d4638d41
Ivan Kniazkov <ivan@kniazkov.com>
2023-11-01 21:54:17 +0300

    JsonObject ready

--- src/main/java/com/kniazkov/json/JsonObject.java
+++ src/main/java/com/kniazkov/json/JsonObject.java
@@ -5,7 +5,7 @@ import java.util.*;
 /**
  * JSON object.
  */
-public class JsonObject extends JsonContainer implements Map<String, JsonElement> {
+public final class JsonObject extends JsonContainer implements Map<String, JsonElement> {
     /**
      * Collection of elements.
      */
@@ -40,66 +40,178 @@ public class JsonObject extends JsonContainer implements Map<String, JsonElement
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return keys.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        return elements.containsKey(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        return elements.containsValue(value);
     }
 
     @Override
     public JsonElement get(Object key) {
-        return null;
+        return elements.get(key);
     }
 
     @Override
     public JsonElement put(String key, JsonElement value) {
-        return null;
+        if (!elements.containsKey(key)) {
+            keys.add(key);
+        }
+        return elements.put(key, value.clone(this));
     }
 
     @Override
     public JsonElement remove(Object key) {
-        return null;
+        JsonElement elem = elements.remove(key);
+        if (elem != null) {
+            keys.remove(key);
+        }
+        return elem;
     }
 
     @Override
-    public void putAll(Map<? extends String, ? extends JsonElement> m) {
-
+    public void putAll(Map<? extends String, ? extends JsonElement> map) {
+        for (Map.Entry<? extends String, ? extends JsonElement> pair : map.entrySet()) {
+            put(pair.getKey(), pair.getValue());
+        }
     }
 
     @Override
     public void clear() {
-
+        elements.clear();
+        keys.clear();
     }
 
     @Override
     public Set<String> keySet() {
-        return null;
+        return elements.keySet();
     }
 
     @Override
     public Collection<JsonElement> values() {
-        return null;
+        return elements.values();
     }
 
     @Override
     public Set<Entry<String, JsonElement>> entrySet() {
-        return null;
+        return elements.entrySet();
     }
 
     @Override
     JsonElement clone(JsonContainer anotherParent) {
-        return null;
+        JsonObject copy = new JsonObject(anotherParent);
+        for (String key : keys) {
+            copy.addElement(key, elements.get(key));
+        }
+        return copy;
     }
 
     @Override
     public String toString() {
-        return null;
+        StringBuilder result = new StringBuilder();
+        result.append('{');
+        boolean flag = false;
+        for (String key : keys) {
+            if (!flag) {
+                result.append(", ");
+            }
+            result.append('"').append(Utils.escapeEntities(key)).append("\": ")
+                    .append(elements.get(key).toString());
+        }
+        result.append('}');
+        return result.toString();
+    }
+
+    /**
+     * Returns a child element by its key.
+     * @param key Key
+     * @return Child element
+     */
+    public JsonElement getElement(String key) {
+        return get(key);
+    }
+
+    /**
+     * Makes a copy of a JSON element and adds a key-value pair to the object.
+     * @param key Key
+     * @param elem Element
+     */
+    public void addElement(String key, JsonElement elem) {
+        put(key, elem);
+    }
+
+    /**
+     * Creates a child element of the 'null' type and adds a key-value pair to the object.
+     * @param key Key
+     */
+    public void addNull(String key) {
+        addChild(key, new JsonNull(this));
+    }
+
+    /**
+     * Creates a child element of the 'boolean' type and adds a key-value pair to the object.
+     * @param key Key
+     @param value Boolean value
+     */
+    public void addBoolean(String key, boolean value) {
+        addChild(key, new JsonBoolean(this, value));
+    }
+
+    /**
+     * Creates a child element of the 'number' type and adds a key-value pair to the object.
+     * @param key Key
+     * @param value Value of the number
+     */
+    public void addNumber(String key, double value) {
+        addChild(key, new JsonNumber(this, value));
+    }
+
+    /**
+     * Creates a child element of the 'string' type and adds a key-value pair to the object.
+     * @param key Key
+     * @param value Value of the string
+     */
+    public void addString(String key, String value) {
+        addChild(key, new JsonString(this, value));
+    }
+
+    /**
+     * Creates an empty array as a child of this object.
+     * @param key Key
+     * @return The empty array created, so that it can be filled.
+     */
+    public JsonArray createArray(String key) {
+        JsonArray child = new JsonArray(this);
+        addChild(key, child);
+        return child;
+    }
+
+    /**
+     * Creates an empty object as a child of this object.
+     * @param key Key
+     * @return The empty object created, so that it can be filled.
+     */
+    public JsonObject createObject(String key) {
+        JsonObject child = new JsonObject(this);
+        addChild(key, child);
+        return child;
+    }
+
+    /**
+     * Adds a child element (for internal use).
+     * @param key Key
+     * @param elem Element
+     */
+    void addChild(String key, JsonElement elem) {
+        if (!elements.containsKey(key)) {
+            keys.add(key);
+        }
+        elements.put(key, elem);
     }
 }
