55140652e5f53da8864d7a05ec079dc70423c1df
Ivan Kniazkov <ivan@kniazkov.com>
2023-11-16 11:09:59 +0300

    Filling String type field

--- src/test/java/com/kniazkov/json/JsonObjectTest.java
+++ src/test/java/com/kniazkov/json/JsonObjectTest.java
@@ -20,8 +20,8 @@ public class JsonObjectTest {
     @Test
     public void objectWithData() {
         JsonObject obj = createTestObject();
-        Assert.assertEquals(5, obj.size());
-        Assert.assertEquals("{\"bool\": true, \"null\": null, \"number\": 13, \"array\": [1, 2], \"obj\": {\"str\": \"test\"}}", obj.toString());
+        Assert.assertEquals(6, obj.size());
+        Assert.assertEquals("{\"bool\": true, \"nothing\": null, \"str\": \"hello\", \"number\": 13, \"array\": [1, 2], \"obj\": {\"str\": \"test\"}}", obj.toString());
     }
 
     @Test
@@ -43,7 +43,21 @@ public class JsonObjectTest {
         private boolean bool;
         private int number;
 
+        private String str;
+
         public TestClass() {
+        }
+
+        public boolean getBool() {
+            return bool;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getString() {
+            return str;
         }
     }
 
@@ -51,6 +65,9 @@ public class JsonObjectTest {
     public void objectToJavaClass() {
         TestClass obj = createTestObject().toObject(TestClass.class);
         Assert.assertNotNull(obj);
+        Assert.assertTrue(obj.getBool());
+        Assert.assertEquals(13, obj.getNumber());
+        Assert.assertEquals("hello", obj.getString());
     }
 
     /**
@@ -60,7 +77,8 @@ public class JsonObjectTest {
     private JsonObject createTestObject() {
         JsonObject obj = new JsonObject();
         obj.addBoolean("bool", true);
-        obj.addNull("null");
+        obj.addNull("nothing");
+        obj.addString("str", "hello");
         obj.addNumber("number", 13);
         JsonArray array = obj.createArray("array");
         array.addNumber(1);
