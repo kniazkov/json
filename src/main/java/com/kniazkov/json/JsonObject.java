b91d6f1f52368da1a8d29a474df17dc346902b52
Ivan Kniazkov <ivan@kniazkov.com>
2023-11-28 09:29:59 +0300

    Parsing and serializing lists (tests)

--- src/main/java/com/kniazkov/json/JsonObject.java
+++ src/main/java/com/kniazkov/json/JsonObject.java
@@ -2,8 +2,6 @@ package com.kniazkov.json;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.*;
 
 /**
